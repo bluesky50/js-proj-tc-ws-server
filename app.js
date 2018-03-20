23391a9fc022074dbd01c0b1b0be8e81870bc34b
Jonathan Chiang <jcdev@Jonathans-Mac-mini.local>
2018-03-20 09:49:11 -0500

    updated connection for heroku

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const { SERVER_PORT } = require('./config/serverConfig.js');
 const port = process.env.PORT || SERVER_PORT;
 
 if (process.env.NODE_ENV === 'test') {
-    server.listen(port, '10.10.10.4', () => {
+    server.listen(port, () => {
         console.log('Server starting in test configuation');
         console.log(`Server started on port ${port}`);
     });
 } else {
-    server.listen(port, '10.10.10.4', () => {
+    server.listen(port, () => {
         console.log('Server starting in non-test configuation');
         console.log(`Server started on port ${port}`);
     });
